f7e70a5bc924df1db8f698c553745bc1157cdd1e
Deepak Edakkott <v-dedakkott@egencia.com>
2021-07-20 12:40:42 +0530

    Output | task-x | given file with pipe

--- py/extract_csv.py
+++ py/extract_csv.py
@@ -3,7 +3,7 @@ import csv
 from multiprocessing import Pool, cpu_count
 
 f1=open("/Users/a-5156/DeepShared/Turnkey/output/3x5K_1.txt", "r", newline='') #todo : absolute , encoding='utf8'
-f2=open("/Users/a-5156/DeepShared/Turnkey/output/3x5K_out.txt", "w")
+f2=open("/Users/a-5156/DeepShared/Turnkey/output/3x5K_out.csv", "w")
 
 #self = {'file' : "/Users/a-5156/DeepShared/Turnkey/output/3x5K_1.txt"}
 
@@ -17,8 +17,9 @@ def replaceSpaceAndNewLine():
     data1 = []
     for line in f1:
         x = line.replace('%@_@%', '|').replace('_@%@_', '\n').splitlines()
-        data1.append(x)
-        #print(x)
+        y = list(filter(None, x))
+        data1.append(y)
+        #print('line : ', y)
         return data1
 
 theData = replaceSpaceAndNewLine() #read_file()
@@ -42,14 +43,30 @@ def main():
     
     #with open(f2, 'a') as outcsv:   
     #configure writer to write standard csv file
-        writer = csv.writer(f2, delimiter='|', quotechar='|', quoting=csv.QUOTE_MINIMAL, lineterminator='\n')
-        writer.writerow(['number', 'text', 'number'])
+        writer = csv.writer(f2, delimiter='|', quoting=csv.QUOTE_NONE, lineterminator='\n')
+        writer.writerow(['col_1', 'col_2', 'col_3', 'col_4', 'col_5', 'col_6', 'col_7'])
         #for item in theData:
             #print(item)   
             #writer.writerow([item])
-        for i in range(0, len(theData[0])):
-            print(theData[0][i])
-            writer.writerow(theData[0][i])
+        #print(theData)  
+        nonEmptyData = list(filter(None, theData))[0]
+        totalLength = len(nonEmptyData)
+        #writer.writerow(nonEmptyData)  
+        print('lenght : ', totalLength)    
+        for i in range(0, totalLength):
+            row = nonEmptyData[i]
+            splitted = row.split('|')
+            print('i val : ', i, '\nrow::::::::: ', splitted)
+            #print(nonEmptyData)
+            c1 = splitted[0]
+            c2 = splitted[1]
+            c3 = splitted[2]
+            c4 = splitted[3]
+            c5 = splitted[4]
+            c6 = splitted[5]
+            c7 = splitted[6]
+            joined = [c1, c2, c3, c4, c5, c6, c7]
+            writer.writerow(joined)
 
 f1.close()
 f2.close()
